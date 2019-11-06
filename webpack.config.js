d38ca11dde191d6143095f0427232dea8a5ead74
Philip Van Raalte <phil.vanraalte@gmail.com>
2019-11-05 20:31:41 -0500

    updated react

--- webpack.config.js
+++ webpack.config.js
@@ -1,38 +1,38 @@
-const webpack = require('webpack');
+const webpack = require("webpack");
 
 module.exports = {
-  entry: [
-    './src/index.js'
-  ],
+  entry: ["./src/index.js"],
   output: {
     path: __dirname,
-    publicPath: '/',
-    filename: 'bundle.js'
+    publicPath: "/",
+    filename: "bundle.js"
   },
   module: {
-    loaders: [{
-      exclude: /node_modules/,
-      loader: 'babel-loader',
-      query: {
-        presets: ['react', 'es2015', 'stage-1']
+    loaders: [
+      {
+        exclude: /node_modules/,
+        loader: "babel-loader",
+        query: {
+          presets: ["react", "es2015", "stage-1"]
+        }
+      },
+      {
+        test: /\.css$/,
+        loader: "style!css!"
       }
-    },
-    {
-      test:/\.css$/,loader:'style!css!'
-    }]
+    ]
   },
   resolve: {
-    extensions: ['.js', '.jsx']
+    extensions: [".js", ".jsx"]
   },
   devServer: {
     historyApiFallback: true,
-    port: 8083,
-    contentBase: './'
+    port: 8080,
+    contentBase: "./"
   },
   plugins: [
-      new webpack.DefinePlugin({
-          //production
-          'process.env.NODE_ENV': JSON.stringify('production') //JSON.stringify(process.env.NODE_ENV || 'development')
-      }),
+    new webpack.DefinePlugin({
+      "process.env.NODE_ENV": JSON.stringify("production")
+    })
   ]
 };
